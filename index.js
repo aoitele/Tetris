32c70c1b6a4c8c2aadd72f989a601bab77d6919e
Takuto Eto <aoitele@gmail.com>
2019-02-13 20:37:22 +0900

    レンダリング追加

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-app.use(express.static("static"));
-const fs = require("fs");
-const path = require("path");
-const htmlPath = path.join(__dirname, "./static/index.html");
-innerhtml = fs.readFileSync(htmlPath, "utf8");
+app.use(express.static('static'));
+const fs = require('fs');
+const path = require('path');
+const htmlPath = path.join(__dirname, './static/index.html');
+innerhtml = fs.readFileSync(htmlPath, 'utf8');
 
-app.get("/", (req, res) => {
-  console.log("YES!!!");
+// ゲームの状態を入れる配列
+const field = [];
+
+// ブロックが出てくる位置
+const init = { x: 3, y: 20 };
+// ブロック7パターンを定義
+// prettier-ignore
+const block = [
+    { init, b1:{x:init.x+1, y:init.y }, b2:{x:init.x+2, y:init.y }, b3:{x:init.x+3, y:init.y }, color:"lightblue" },
+    { init, b1:{x:init.x, y:init.y+1 }, b2:{x:init.x+1, y:init.y }, b3:{x:init.x+2, y:init.y }, color:"blue" },
+    { init, b1:{x:init.x+1, y:init.y }, b2:{x:init.x+2, y:init.y }, b3:{x:init.x+2, y:init.y+1 }, color:"orange" },
+    { init, b1:{x:init.x, y:init.y+1 }, b2:{x:init.x+1, y:init.y }, b3:{x:init.x+1, y:init.y+1 }, color:"yellow" },
+    { init, b1:{x:init.x+1, y:init.y }, b2:{x:init.x+1, y:init.y+1 }, b3:{x:init.x+2, y:init.y+1 }, color:"green" },
+    { init, b1:{x:init.x+1, y:init.y }, b2:{x:init.x+1, y:init.y+1 }, b3:{x:init.x+2, y:init.y }, color:"purple" },
+    { init, b1:{x:init.x+1, y:init.y }, b2:{x:init.x+1, y:init.y-1 }, b3:{x:init.x+2, y:init.y-1 }, color:"red" }
+];
+
+app.get('/', (req, res) => {
+  console.log('root...');
 });
 
-app.get("/backend", (req, res) => {});
+app.get('/field', (req, res) => {
+  console.log('connecting...');
+});
 
 app.listen(8000);
